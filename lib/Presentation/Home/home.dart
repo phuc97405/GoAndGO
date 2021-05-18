30c4978c8fb21eb7b62c582595b25455c4cb012f
PhucLee <phuc97405@gmail.com>
2021-05-18 17:27:49 +0700

    fix map

--- lib/Presentation/Home/home.dart
+++ lib/Presentation/Home/home.dart
@@ -1,26 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:goandgo/Presentation/Home/home_controller.dart';
-import 'package:goandgo/Presentation/My_App/app_controller.dart';
 import 'package:goandgo/Presentation/constants.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends GetView<HomeController> {
+  Completer<GoogleMapController> controllerGoogle = Completer();
   Widget build(BuildContext context) {
-    final controllerApp = Get.find<AppController>();
+    // final controllerApp = Get.find<AppController>();
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text('Home'),
         actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
       ),
-      body:
-          //  Expanded(
-          //   child:
-          new TextButton(
-        onPressed: () {
-          controllerApp.logOut();
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: controller.kGooglePlex,
+        onMapCreated: (GoogleMapController controllergoogle) {
+          controllerGoogle.complete(controllergoogle);
         },
-        child: new Text('Logout'),
       ),
       // ),
       floatingActionButton: FloatingActionButton(
@@ -28,22 +29,26 @@ class HomePage extends GetView<HomeController> {
         backgroundColor: kPrimaryColor,
         child: Icon(Icons.location_searching),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: controller.selectionIndex.value,
-        onTap: (value) => {controller.selectionIndex = value.obs},
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
-          BottomNavigationBarItem(icon: Icon(Icons.call), label: "Call"),
-          BottomNavigationBarItem(
-              icon: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/user.jpg'),
-                radius: 14,
-              ),
-              label: 'Profile')
-        ],
-      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: controller.selectionIndex.value,
+      onTap: (value) => {controller.selectionIndex.value = value},
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+        BottomNavigationBarItem(icon: Icon(Icons.call), label: "Call"),
+        BottomNavigationBarItem(
+            icon: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/user.jpg'),
+              radius: 14,
+            ),
+            label: 'Profile')
+      ],
     );
   }
 }
--- lib/Presentation/Home/home_controller.dart
+++ lib/Presentation/Home/home_controller.dart
@@ -1,7 +1,20 @@
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeController extends GetxController {
   RxInt selectionIndex = 0.obs;
+
+  final CameraPosition kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  final CameraPosition kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   void onInit() {
     super.onInit();
